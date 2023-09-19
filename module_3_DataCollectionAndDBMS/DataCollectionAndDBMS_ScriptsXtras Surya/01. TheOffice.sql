228962a961966866388be817abfec4bd2e7e94af
jamwalshah <suryadevsinghjamwal01@gmail.com>
2023-09-19 15:00:52 +0530

    added SQL scripts Xtras

---
+++ module_3_DataCollectionAndDBMS/DataCollectionAndDBMS_ScriptsXtras Surya/01. TheOffice.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS TheOffice ;
+USE TheOffice;
+
+CREATE TABLE EmployeeDemographics (
+EmployeeID INT,
+FirstName VARCHAR(50),
+LastName VARCHAR(50),
+Age INT,
+Gender VARCHAR(50)
+);
+
+CREATE TABLE EmployeeSalary(
+	EmployeeID INT,
+    JobTitle VARCHAR(50),
+    Salary INT
+);
+
+INSERT INTO EmployeeDemographics VALUES
+(1001, 'Jim', 'Halpert', 30, 'Male'),
+(1002, 'Pam', 'Beasley', 30, 'Female'),
+(1003, 'Dwight', 'Schrute', 29, 'Male'),
+(1004, 'Angela', 'Martin', 31, 'Female'),
+(1005, 'Toby', 'Flenderson', 32, 'Male'),
+(1006, 'Michael', 'Scott', 35, 'Male'),
+(1007, 'Meredith', 'Palmer', 32, 'Female'),
+(1008, 'Stanley', 'Hudson', 38, 'Male'),
+(1009, 'Kevin', 'Malone', 31, 'Male');
+
+INSERT INTO EmployeeSalary VALUES
+(1001, 'Salesman', 45000),
+(1002, 'Receptionist', 36000),
+(1003, 'Salesman', 63000),
+(1004, 'Accountant', 47000),
+(1005, 'HR', 50000),
+(1006, 'Regional Manager', 65000),
+(1007, 'Supplier Relations', 41000),
+(1008, 'Salesman', 48000),
+(1009, 'Accountant', 42000);
+
+SELECT * FROM EmployeeDemographics ;
+SELECT * FROM EmployeeSalary;
