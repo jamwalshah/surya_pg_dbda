e4c0a293dfaf0a44d7bfd19c36220e4170cd90c5
Surya Dev Singh Jamwal <suryadevsinghjamwal01@gmail.com>
2023-10-22 15:28:52 +0530

    Added BDT Day13 files

---
+++ module_5_BigDataTechnologies/BigDataTechnologies_EclipseIDEWorkspace Surya/13.06 UnixtimeToDate.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Date;
+import java.text.DateFormat;
+import org.apache.hadoop.hive.ql.exec.UDF; 
+import org.apache.hadoop.io.Text;
+
+public class UnixtimeToDate extends UDF{
+	public Text evaluate(Text text){
+		if(text==null) return null;
+		long timestamp = Long.parseLong(text.toString());
+		return new Text(toDate(timestamp));
+	}
+	private String toDate(long timestamp) {
+		Date date = new Date (timestamp);
+		return DateFormat.getInstance().format(date).toString();
+	}
+}
