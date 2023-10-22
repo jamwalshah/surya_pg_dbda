fde165601a81610ecf65f9e2ca79cfceab6db611
Surya Dev Singh Jamwal <suryadevsinghjamwal01@gmail.com>
2023-10-22 13:04:47 +0530

    added BDT workspace files in Eclipse IDE by Surya

---
+++ module_5_BigDataTechnologies/BigDataTechnologies_Files Surya/Hadoop-CDAC/src/AllTimeLow.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+
+
+public class AllTimeLow {
+	
+	public static class MapClass extends Mapper<LongWritable,Text,Text,DoubleWritable>
+	   {
+	      public void map(LongWritable key, Text value, Context context)
+	      {	    	  
+	         try{
+	            String[] str = value.toString().split(",");	 
+	            double low = Double.parseDouble(str[5]);
+	            context.write(new Text(str[1]),new DoubleWritable(low));
+	         }
+	         catch(Exception e)
+	         {
+	            System.out.println(e.getMessage());
+	         }
+	      }
+	   }
+	
+	  public static class ReduceClass extends Reducer<Text,DoubleWritable,Text,DoubleWritable>
+	   {
+		    private DoubleWritable result = new DoubleWritable();
+		    
+		    public void reduce(Text key, Iterable<DoubleWritable> values,Context context) throws IOException, InterruptedException {
+		      double min = Double.MAX_VALUE;
+				
+		         for (DoubleWritable val : values)
+		         {       	
+//		        	sum += val.get();
+		        	 if (val.get()<min) {
+		        		 min = val.get();
+		        	 }
+		        	 
+		         }
+		         
+		      result.set(min);		      
+		      context.write(key, result);
+		      //context.write(key, new LongWritable(sum));
+		      
+		    }
+	   }
+	  public static void main(String[] args) throws Exception {
+		    Configuration conf = new Configuration();
+		    //conf.set("name", "value")
+		    //conf.set("mapreduce.input.fileinputformat.split.minsize", "134217728");
+		    Job job = Job.getInstance(conf, "All Time High  price for each stock");
+		    job.setJarByClass(AllTimeHigh.class);
+		    job.setMapperClass(MapClass.class);
+		    //job.setCombinerClass(ReduceClass.class);
+		    job.setReducerClass(ReduceClass.class);
+		    job.setNumReduceTasks(1); // means it will run one reducer only
+//		    job.setNumReduceTasks(0); // means no reducer will run
+		    job.setOutputKeyClass(Text.class);
+		    job.setOutputValueClass(DoubleWritable.class);
+		    FileInputFormat.addInputPath(job, new Path(args[0]));
+		    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		    System.exit(job.waitForCompletion(true) ? 0 : 1);
+		  }
+}
---
+++ module_5_BigDataTechnologies/BigDataTechnologies_Files Surya/Hadoop-CDAC/src/ReduceJoin.java
@@ -0,0 +1,76 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ReduceJoin {
+
+	public static class CustsMapper extends
+			Mapper<LongWritable, Text, Text, Text> {
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String record = value.toString();
+			String[] parts = record.split(",");
+			context.write(new Text(parts[0]), new Text("custs\t" + parts[1]));
+		}
+	}
+
+	public static class TxnsMapper extends
+			Mapper<LongWritable, Text, Text, Text> {
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String record = value.toString();
+			String[] parts = record.split(",");
+			context.write(new Text(parts[2]), new Text("txns\t" + parts[3]));
+		}
+	}
+
+	public static class ReduceJoinReducer extends
+			Reducer<Text, Text, Text, Text> {
+		public void reduce(Text key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+			String name = "";
+			double total = 0.0;
+			int count = 0;
+			for (Text t : values) {
+				String parts[] = t.toString().split("\t");
+				if (parts[0].equals("txns")) {
+					count++;
+					total += Float.parseFloat(parts[1]);
+				} else if (parts[0].equals("custs")) {
+					name = parts[1];
+				}
+			}
+			String str = String.format("%d\t%f", count, total);
+			context.write(new Text(name), new Text(str));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf);
+	    job.setJarByClass(ReduceJoin.class);
+	    job.setJobName("Reduce Side Join");
+		job.setReducerClass(ReduceJoinReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		job.setNumReduceTasks(1);
+		MultipleInputs.addInputPath(job, new Path(args[0]),TextInputFormat.class, CustsMapper.class);
+		MultipleInputs.addInputPath(job, new Path(args[1]),TextInputFormat.class, TxnsMapper.class);
+		
+		Path outputPath = new Path(args[2]);
+		FileOutputFormat.setOutputPath(job, outputPath);
+		//outputPath.getFileSystem(conf).delete(outputPath);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
